e5d8b4808563c426bc76742d6d05aadd216042d9
Antoine MUTHIG <amuthig@consultants-solutec.fr>
2024-11-26 09:21:20 +0100

    main architecture

---
+++ backend/src/main/java/com/fantastik/fantastik/FantastikApplication.java
@@ -0,0 +1,13 @@
+package com.fantastik.fantastik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantastikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantastikApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/fantastik/fantastik/FantastikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fantastik.fantastik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantastikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
